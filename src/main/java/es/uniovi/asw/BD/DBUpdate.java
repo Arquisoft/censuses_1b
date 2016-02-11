836ad4b0dedd4c67c52c48c6137bd48851e03d97
Xurxo García <xurxo9910@gmail.com>
2016-02-11 21:43:33 +0100

    Implementada la conexión con la BD

--- src/main/java/es/uniovi/asw/BD/DBUpdate.java
+++ src/main/java/es/uniovi/asw/BD/DBUpdate.java
@@ -1,10 +1,51 @@
 package es.uniovi.asw.BD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import es.uniovi.asw.logica.Votante;
 
+/**
+ * Clase para interactuar con la base de datos
+ *
+ */
 public class DBUpdate {
 	
-	public void Insert(Votante v){
+	private static String DRIVER = "org.hsqldb.jdbcDriver";
+	private static String URL = "jdbc:hsqldb:hsql://localhost";
+	private static String USER = "sa";
+	private static String PASS = "";
+	
+	private static Connection con;
+	
+	/**
+	 * Método para establecer una conexión con la BD
+	 */
+	public void conectar () {
+		try {
+			Class.forName("DRIVER");
+			} catch (ClassNotFoundException e){
+				throw new RuntimeException("No se ha podido cargar el driver!", e);
+				}
+		try {
+			con = DriverManager.getConnection(URL, USER, PASS);
+		} catch (SQLException e) {
+			throw new RuntimeException("No se ha podido establecer la conexión!", e);
+		}		
+	}
+
+	/**
+	 * Método para desconectarse de la BD
+	 */
+	public void desconectar() {
+		if (con != null) 
+			try { 
+				con.close();
+				} catch(SQLException e) {};
+	}
+
+	public void insert(Votante v){
 		
 	}
 
