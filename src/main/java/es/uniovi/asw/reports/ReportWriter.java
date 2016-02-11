b00cbb23288ab0c85b69cd7e9e89a74fad2e297f
David Sánchez <davidslufe02@gmail.com>
2016-02-11 21:40:02 +0100

    Report Writtter con su junit
    Funciona correctamente

--- src/main/java/es/uniovi/asw/reports/ReportWriter.java
+++ src/main/java/es/uniovi/asw/reports/ReportWriter.java
@@ -1,11 +1,67 @@
 package es.uniovi.asw.reports;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.io.FileWriter;
+import java.util.Calendar;
+
+
+
+
+
 import es.uniovi.asw.logica.Votante;
 
 public class ReportWriter {
 	
-	public void WriteReport(Votante v, String razon){
+	public void WriteReport(Votante v,String ficheroExcelProcedencia, String razon) throws IOException{
+		if(v==null ||ficheroExcelProcedencia==null || razon==null){
+			throw new IllegalArgumentException("Parametros no pueden ser null");
+		}
+		if(ficheroExcelProcedencia.isEmpty() || razon.isEmpty()){
+			throw new IllegalArgumentException("Informacion sobre la razon del reporte y el origen del fichero necesaria");
+		}
+		Writer output;
+		output = new BufferedWriter(new FileWriter("report.log", true));
 		
+		Calendar hoy= Calendar.getInstance();
+			
+		int anyo=hoy.get(Calendar.YEAR) - 1900;
+		int mes = hoy.get(Calendar.MONTH)+1;
+		int dia= hoy.get(Calendar.DAY_OF_MONTH);
+		
+		int hora= hoy.get(Calendar.HOUR);
+		int minuto= hoy.get(Calendar.MINUTE);
+		int segundo= hoy.get(Calendar.SECOND);
+		
+		String report="["+(anyo)+"/"+mes+"/"+dia+"::"+hora+":"+minuto+":"+segundo+ "]@"+ficheroExcelProcedencia+" votante con problemas:"+v.getNif()+" Razon: "+razon+"\n" ;
+		output.write(report);
+		output.close();
+	}
+	
+	public void WriteReport(String ficheroExcelProcedencia,String razon) throws IOException{
+		if(ficheroExcelProcedencia==null || razon==null){
+			throw new IllegalArgumentException("Parametros no pueden ser null");
+		}
+		if(ficheroExcelProcedencia.isEmpty() || razon.isEmpty()){
+			throw new IllegalArgumentException("Informacion sobre la razon del reporte y el origen del fichero necesaria");
+		}
+		Writer output;
+		output = new BufferedWriter(new FileWriter("report.log", true));
+		
+		Calendar hoy= Calendar.getInstance();
+			
+		int anyo=hoy.get(Calendar.YEAR) - 1900;
+		int mes = hoy.get(Calendar.MONTH)+1;
+		int dia= hoy.get(Calendar.DAY_OF_MONTH);
+		
+		int hora= hoy.get(Calendar.HOUR);
+		int minuto= hoy.get(Calendar.MINUTE);
+		int segundo= hoy.get(Calendar.SECOND);
+		
+		String report="["+(anyo)+"/"+mes+"/"+dia+"::"+hora+":"+minuto+":"+segundo+ "] @ "+ficheroExcelProcedencia+" Problema debido a: "+razon +"\n";
+		output.write(report);
+		output.close();
 	}
 
 }
