35e91993d52fb0c4b3d70728e01fc5288a9f9086
Santiago Martin <santiagomartinagra@gmail.com>
2016-02-11 16:12:49 +0100

    Tests para ReadCensusExcel

---
+++ src/test/java/es/uniovi/asw/ReadCensusExcelTest.java
@@ -0,0 +1,87 @@
+package es.uniovi.asw;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import es.uniovi.asw.logica.Votante;
+import es.uniovi.asw.passer.ReadCensus;
+import es.uniovi.asw.passer.impl.ReadCensusExcel;
+
+public class ReadCensusExcelTest {
+
+	@Test
+	public void testExcelModernoNumeroVotantes() throws Exception {
+		ReadCensus readCensus = new ReadCensusExcel();
+		List<Votante> votantes = readCensus.loadCenso("src/test/resources/censo_correcto.xlsx"); 
+		
+		//Comprobamos número de votantes
+		assertEquals(votantes.size(), 3);
+	}
+	
+	@Test
+	public void testExcelAntiguoNumeroVotantes() throws Exception {
+		ReadCensus readCensus = new ReadCensusExcel();
+		List<Votante> votantes = readCensus.loadCenso("src/test/resources/censo_correcto.xls"); 
+		
+		//Comprobamos número de votantes
+		assertEquals(votantes.size(), 3);
+	}
+	
+	@Test
+	public void testExcelModernoDatosCorrectos() throws Exception {
+		ReadCensus readCensus = new ReadCensusExcel();
+		List<Votante> votantes = readCensus.loadCenso("src/test/resources/censo_correcto.xlsx"); 
+		
+		//Comprobamos los datos del votante 1
+		Votante votante1 = votantes.get(0);
+		assertTrue(votante1.getNombre().equals("Pepe"));
+		assertTrue(votante1.getMail().equals("pepe@yomolomucho.es"));
+		assertTrue(votante1.getNif().equals("65432123A"));
+		assertTrue(votante1.getCodigoColegio().equals("32.0"));
+		
+		//Comprobamos los datos del votante 2
+		Votante votante2 = votantes.get(1);
+		assertTrue(votante2.getNombre().equals("Juan"));
+		assertTrue(votante2.getMail().equals("juan@gmail.com"));
+		assertTrue(votante2.getNif().equals("75643234W"));
+		assertTrue(votante2.getCodigoColegio().equals("43.0"));
+
+		//Comprobamos los datos del votante 3
+		Votante votante3 = votantes.get(2);
+		assertTrue(votante3.getNombre().equals("Sergio"));
+		assertTrue(votante3.getMail().equals("sergio@yomolomucho.es"));
+		assertTrue(votante3.getNif().equals("12321543P"));
+		assertTrue(votante3.getCodigoColegio().equals("54.0"));
+	}
+	
+	@Test
+	public void testExcelAntiguoDatosCorrectos() throws Exception {
+		ReadCensus readCensus = new ReadCensusExcel();
+		List<Votante> votantes = readCensus.loadCenso("src/test/resources/censo_correcto.xls"); 
+		
+		//Comprobamos los datos del votante 1
+		Votante votante1 = votantes.get(0);
+		assertTrue(votante1.getNombre().equals("Pepe"));
+		assertTrue(votante1.getMail().equals("pepe@yomolomucho.es"));
+		assertTrue(votante1.getNif().equals("65432123A"));
+		assertTrue(votante1.getCodigoColegio().equals("32.0"));
+
+		//Comprobamos los datos del votante 2
+		Votante votante2 = votantes.get(1);
+		assertTrue(votante2.getNombre().equals("Juan"));
+		assertTrue(votante2.getMail().equals("juan@gmail.com"));
+		assertTrue(votante2.getNif().equals("75643234W"));
+		assertTrue(votante2.getCodigoColegio().equals("43.0"));
+
+		//Comprobamos los datos del votante 3
+		Votante votante3 = votantes.get(2);
+		assertTrue(votante3.getNombre().equals("Sergio"));
+		assertTrue(votante3.getMail().equals("sergio@yomolomucho.es"));
+		assertTrue(votante3.getNif().equals("12321543P"));
+		assertTrue(votante3.getCodigoColegio().equals("54.0"));
+	}
+
+}
